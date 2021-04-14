57f57b065836b62a6e17c707ae0f0c7841563668
rounakrakangor <67686905+rounakrakangor@users.noreply.github.com>
2021-04-14 14:56:10 +0530

    Add files via upload

---
+++ program_1.py
@@ -0,0 +1,15 @@
+# split without using split function
+s = 'rounak rakangor'
+l = []
+nl = []
+ns = ''
+for i in s:
+    if i != ' ':
+        ns = ns+i
+    else:
+        l.append(ns)
+        ns = ''
+l.append(ns)
+print(l)
+nl = l 
+print(nl)
